1b0ca5a605199d5eec6cdb4d6a9cc4665b0cab43
sakshi-builds <sharmasakshi1117@gmail.com>
2026-05-14 15:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 const getSumBtn = document.createElement("button");
+
 getSumBtn.append("Get Total Price");
+
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 
-    // select all price elements
     let prices = document.querySelectorAll(".prices");
 
-    // variable to store total
     let total = 0;
 
-    // loop through all prices
     prices.forEach((i) => {
         total += parseInt(i.textContent);
     });
 
-    // select table
-    const table = document.querySelector("table");
+    // select tbody
+    const tableBody = document.querySelector("tbody");
 
-    // create new row
+    // create row
     const row = document.createElement("tr");
 
-    // create new cell
+    // create td
     const data = document.createElement("td");
 
     // add total text
     data.textContent = "Total Price: " + total;
 
-    // make td span across columns
+    // span across columns
     data.colSpan = 2;
 
-    // append td inside row
+    // append td to row
     row.appendChild(data);
 
-    // append row inside table
-    table.appendChild(row);
+    // append row to tbody
+    tableBody.appendChild(row);
 };
 
-// button click event
 getSumBtn.addEventListener("click", getSum);
